cc84b0e4367f6358660262f0b752d4eadbeb8dc0
biio-front <bohwajung@naver.com>
2022-02-13 22:12:13 +0900

    Add function to change text color and position

---
+++ src/functions/canvas.ts
@@ -0,0 +1,37 @@
+import { CanvasElement } from '../type/canvas';
+
+export const getCanvasItemPosition = (event: React.DragEvent<HTMLInputElement>) => {
+  const canvasElement = document.querySelector('#canvas')?.getBoundingClientRect();
+
+  const startX = canvasElement?.left || 0;
+  const startY = canvasElement?.top || 0;
+
+  const posX = event.clientX;
+  const posY = event.clientY;
+
+  return { posX: posX - startX, posY: posY - startY };
+};
+
+export const changeElement = ({
+  elements,
+  selectedItem,
+  changedValues,
+}: {
+  elements: CanvasElement[];
+  selectedItem: CanvasElement;
+  changedValues: { [k in string]: string | number };
+}) => {
+  const selectedIndex = Number(selectedItem.id);
+  const changedSelectedElement = {
+    ...selectedItem,
+    styles: {
+      ...selectedItem.styles,
+      ...changedValues,
+    },
+  };
+
+  const changedElements = [...elements];
+  changedElements.splice(selectedIndex, 1, changedSelectedElement);
+
+  return changedElements;
+};
